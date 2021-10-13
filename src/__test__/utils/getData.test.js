fb7f8515126b1459c9dc2dd423b22cf564835771
Jose Marquinez <josemarquinez28@gmail.com>
2021-10-13 14:19:28 -0500

    :bug: Fixing the Fetch test for an API

--- src/__test__/utils/getData.test.js
+++ src/__test__/utils/getData.test.js
@@ -5,12 +5,13 @@ describe('Fetch API', () => {
     fetch.resetMocks(); // Clear previously set mocks so they do not bleed into other mocks
   });
 
-  test('Call an API and return data', async () => {
+  test('Call an API and return data', (done) => {
     fetch.mockResponseOnce(JSON.stringify({ data: '12345' })); // Mock each fetch call independently
 
     getData('https://google.com')
       .then((data) => {
-        return expect(data.data).toEqual('1234');
+        expect(data.data).toEqual('12345');
+        done(); // Jest will wait until the done callback is called before finishing the test.
       });
 
     expect(fetch.mock.calls[0][0]).toEqual('https://google.com');
